ce7a8f8be1f93cc71503f9382d64390d1107eb09
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-08-05 18:13:33 -0300

    agrego configapp

---
+++ config/configapp.php
@@ -0,0 +1,7 @@
+<?php
+
+// constantes pertenecientes a la conexión con DB
+const SQL_USER = "root";
+const SQL_PASS = "";
+const SQL_DBNAME = "webii_productstpe;charset=utf8";
+const SQL_HOST = "localhost";
