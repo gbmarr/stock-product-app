9ecbe00fb26d28d3d2eb1034f38a67ad80cdc2cb
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-11-20 16:25:28 -0300

    modifico codigo SQL en database para utilizar INSERTS y elimino logica innecesaria en Model

--- app/model/Model.php
+++ app/model/Model.php
@@ -26,46 +26,46 @@ class Model{
 
             $pdo->exec($sql);
 
-            $this->database = new PDO("mysql:host=".SQL_HOST.";dbname=".SQL_DBNAME."", SQL_USER, SQL_PASS);
-            if($this->isTableEmpty('categories') && $this->isTableEmpty('products') && $this->isTableEmpty('users')){
-                $this->insertData();
-            }
+            // $this->database = new PDO("mysql:host=".SQL_HOST.";dbname=".SQL_DBNAME."", SQL_USER, SQL_PASS);
+            // if($this->isTableEmpty('categories') && $this->isTableEmpty('products') && $this->isTableEmpty('users')){
+            //     $this->insertData();
+            // }
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
 
-    private function isTableEmpty($table){
-        $sql = "SELECT COUNT(*) FROM $table";
-        $isCharge = $this->database->prepare($sql);
-        $isCharge->execute();
-        return $isCharge->fetchColumn() == 0;
-    }
+    // private function isTableEmpty($table){
+    //     $sql = "SELECT COUNT(*) FROM $table";
+    //     $isCharge = $this->database->prepare($sql);
+    //     $isCharge->execute();
+    //     return $isCharge->fetchColumn() == 0;
+    // }
 
-    private function insertData(){
-        try {
-            $this->database->exec(
-                "INSERT INTO `categories` (`name`) VALUES
-                ('Sin categoría asignada'),
-                ('Electrónica'),
-                ('Ropa'),
-                ('Hogar');");
-            $this->database->exec(
-                "INSERT INTO `products` (`name`, `description`, `idcategory`, `stock`, `price`, `imgproduct`) VALUES
-                ('Televisor', 'Televisor LED de 42 pulgadas', 2, TRUE, 200, 'https://th.bing.com/th/id/OIP.3Tdr8z2sWnjazwobaV_qigHaGR?w=222&h=188&c=7&r=0&o=5&dpr=1.3&pid=1.7'),
-                ('Camiseta', 'Camiseta de algodón 100%', 3, TRUE, 30.00, 'https://www.cienporcientofutbol.cl/cdn/shop/files/Camiseta_Local_Seleccion_Argenti_3_840x840.jpg?v=1711568681'),
-                ('Sofá', 'Sofá de 3 plazas color gris', 4, TRUE, 500.10, 'https://i5.walmartimages.com/seo/3-Seater-Sofa-Couch-Modern-Linen-Tufed-Upholstered-2-Pillows-Armrest-Design-Wooden-Tapered-Legs-Accent-Arm-Sofas-Living-Room-Bedroom-Office-Grey_04ec8f14-062d-44d0-9aed-9dac7be853b9.5e2b287cd25bd91421fd9bb39412dbc8.jpeg'),
-                ('Auriculares', 'Auriculares con cancelación de ruido', 2, TRUE, 1500, 'https://www.sevenelectronics.com.ar/images/000000mls-s389ne02151s389-negro1.png');
-            ");
-            $this->database->exec(
-                "INSERT INTO `users` (`name`, `surname`, `email`, `pass`, `admin`) VALUES
-                ('Admin', 'User', 'webadmin@admin.com', 'admin', TRUE),
-                ('UserComun', 'Apellido', 'user@comun.com', 'user', FALSE);"
-            );
-        } catch (Exception $e) {
-            echo $e->getMessage();
-        }
-    }
+    // private function insertData(){
+    //     try {
+    //         $this->database->exec(
+    //             "INSERT INTO `categories` (`name`) VALUES
+    //             ('Sin categoría asignada'),
+    //             ('Electrónica'),
+    //             ('Ropa'),
+    //             ('Hogar');");
+    //         $this->database->exec(
+    //             "INSERT INTO `products` (`name`, `description`, `idcategory`, `stock`, `price`, `imgproduct`) VALUES
+    //             ('Televisor', 'Televisor LED de 42 pulgadas', 2, TRUE, 200, 'images/img-tv.jpeg'),
+    //             ('Camiseta', 'Camiseta de algodón 100%', 3, TRUE, 30.00, 'images/camiseta.jpeg'),
+    //             ('Sofá', 'Sofá de 3 plazas color gris', 4, TRUE, 500.10, 'images/sofa.jpeg'),
+    //             ('Auriculares', 'Auriculares con cancelación de ruido', 2, TRUE, 1500, 'images/auriculares.jpeg');
+    //         ");
+    //         $this->database->exec(
+    //             "INSERT INTO `users` (`name`, `surname`, `email`, `pass`, `admin`) VALUES
+    //             ('Admin', 'User', 'webadmin@admin.com', 'admin', TRUE),
+    //             ('UserComun', 'Apellido', 'user@comun.com', 'user', FALSE);"
+    //         );
+    //     } catch (Exception $e) {
+    //         echo $e->getMessage();
+    //     }
+    // }
 
     function executeQuery($query, $params = []){
         try {
--- data/database.sql
+++ data/database.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE IF NOT EXISTS webii_productstpe;
 USE webii_productstpe;
 
--- Tabla categories
+/* Tabla categories */
 CREATE TABLE IF NOT EXISTS categories (
     idcat INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
--- Tabla products
+/* Tabla products */
 CREATE TABLE IF NOT EXISTS products (
     idproduct INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS products (
     FOREIGN KEY (idcategory) REFERENCES categories(idcat)
 );
 
--- Tabla users
+/* Tabla users */
 CREATE TABLE IF NOT EXISTS users (
     iduser INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -29,65 +29,59 @@ CREATE TABLE IF NOT EXISTS users (
     admin BOOLEAN NOT NULL
 );
 
--- Inserts en tabla CATEGORIES
--- INSERT INTO categories (name)
--- SELECT * FROM (SELECT 'Electrónica') AS tmp
--- WHERE NOT EXISTS (
-    -- SELECT name FROM categories WHERE name = 'Electrónica'
--- ) LIMIT 1;
+/*Inserts en tabla CATEGORIES*/
+INSERT INTO categories (name)
+SELECT * FROM (SELECT 'Electrónica') AS tmp
+WHERE NOT EXISTS (
+    SELECT name FROM categories WHERE name = 'Electrónica'
+) LIMIT 1;
 
--- INSERT INTO categories (name)
--- SELECT * FROM (SELECT 'Ropa') AS tmp
--- WHERE NOT EXISTS (
-    -- SELECT name FROM categories WHERE name = 'Ropa'
--- ) LIMIT 1;
+INSERT INTO categories (name)
+SELECT * FROM (SELECT 'Ropa') AS tmp
+WHERE NOT EXISTS (
+    SELECT name FROM categories WHERE name = 'Ropa'
+) LIMIT 1;
 
--- INSERT INTO categories (name)
--- SELECT * FROM (SELECT 'Hogar') AS tmp
--- WHERE NOT EXISTS (
-    -- SELECT name FROM categories WHERE name = 'Hogar'
--- ) LIMIT 1;
+INSERT INTO categories (name)
+SELECT * FROM (SELECT 'Hogar') AS tmp
+WHERE NOT EXISTS (
+    SELECT name FROM categories WHERE name = 'Hogar'
+) LIMIT 1;
 
--- -- Inserts en tabla PRODUCTS
--- INSERT INTO products (name, description, idcategory, stock, price)
--- SELECT * FROM (SELECT 'Televisor', 'Televisor LED de 42 pulgadas', 1, TRUE, 200) AS tmp
--- WHERE NOT EXISTS (
-    -- SELECT name FROM products WHERE name = 'Televisor'
--- ) LIMIT 1;
+/* Inserts en tabla PRODUCTS */
+INSERT INTO products (name, description, idcategory, stock, price, imgproduct)
+SELECT * FROM (SELECT 'Televisor', 'Televisor LED de 42 pulgadas', 1, TRUE, 200, 'images/img-tv.jpeg') AS tmp
+WHERE NOT EXISTS (
+    SELECT name FROM products WHERE name = 'Televisor'
+) LIMIT 1;
 
--- INSERT INTO products (name, description, idcategory, stock, price)
--- SELECT * FROM (SELECT 'Camiseta', 'Camiseta de algodón 100%', 2, TRUE, 30.00) AS tmp
--- WHERE NOT EXISTS (
-    -- SELECT name FROM products WHERE name = 'Camiseta'
--- ) LIMIT 1;
+INSERT INTO products (name, description, idcategory, stock, price, imgproduct)
+SELECT * FROM (SELECT 'Camiseta', 'Camiseta de algodón 100%', 2, TRUE, 30.00, 'images/camiseta.jpeg') AS tmp
+WHERE NOT EXISTS (
+    SELECT name FROM products WHERE name = 'Camiseta'
+) LIMIT 1;
 
--- INSERT INTO products (name, description, idcategory, stock, price)
--- SELECT * FROM (SELECT 'Sofá', 'Sofá de 3 plazas color gris', 3, TRUE, 500.10) AS tmp
--- WHERE NOT EXISTS (
-    -- SELECT name FROM products WHERE name = 'Sofá'
--- ) LIMIT 1;
+INSERT INTO products (name, description, idcategory, stock, price, imgproduct)
+SELECT * FROM (SELECT 'Sofá', 'Sofá de 3 plazas color gris', 3, TRUE, 500.10, 'images/sofa.jpeg') AS tmp
+WHERE NOT EXISTS (
+    SELECT name FROM products WHERE name = 'Sofá'
+) LIMIT 1;
 
--- INSERT INTO products (name, description, idcategory, stock, price)
--- SELECT * FROM (SELECT 'Auriculares', 'Auriculares con cancelación de ruido', 1, TRUE, 1500) AS tmp
--- WHERE NOT EXISTS (
-    -- SELECT name FROM products WHERE name = 'Auriculares'
--- ) LIMIT 1;
+INSERT INTO products (name, description, idcategory, stock, price, imgproduct)
+SELECT * FROM (SELECT 'Auriculares', 'Auriculares con cancelación de ruido', 1, TRUE, 1500, 'images/auriculares.jpeg') AS tmp
+WHERE NOT EXISTS (
+    SELECT name FROM products WHERE name = 'Auriculares'
+) LIMIT 1;
 
--- -- Inserts en tabla USERS
--- INSERT INTO users (name, surname, email, pass, admin)
--- SELECT * FROM (SELECT 'Admin', 'User', 'admin@admin.com', 'webadmin', TRUE) AS tmp
--- WHERE NOT EXISTS (
-    -- SELECT email FROM users WHERE email = 'admin@admin.com'
--- ) LIMIT 1;
+/* Inserts en tabla USERS */
+INSERT INTO users (name, surname, email, pass, admin)
+SELECT * FROM (SELECT 'Admin', 'User', 'admin@admin.com', '$2y$10$xHn/k/qTE8tGr1iSSe3X0OOUbkSnYGTLp7BAuIKFUAb9teWs/Ybxi', TRUE) AS tmp
+WHERE NOT EXISTS (
+    SELECT email FROM users WHERE email = 'admin@admin.com'
+) LIMIT 1;
 
--- INSERT INTO users (name, surname, email, pass, admin)
--- SELECT * FROM (SELECT 'Gabriel', 'M', 'gb@web.com', 'password123', FALSE) AS tmp
--- WHERE NOT EXISTS (
-    -- SELECT email FROM users WHERE email = 'gb@web.com'
--- ) LIMIT 1;
-
--- INSERT INTO users (name, surname, email, pass, admin)
--- SELECT * FROM (SELECT 'G', 'Marrero', 'gm@web.com', 'password456', FALSE) AS tmp
--- WHERE NOT EXISTS (
-    -- SELECT email FROM users WHERE email = 'gm@web.com'
--- ) LIMIT 1;
+INSERT INTO users (name, surname, email, pass, admin)
+SELECT * FROM (SELECT 'Usuario', 'Comun', 'user@user.com', '$2y$10$hP1MFvzm7SpV6CbZX7GQDekSTWD0GyVtR0eN2icxbLHNM.QfQ0U4a', FALSE) AS tmp
+WHERE NOT EXISTS (
+    SELECT email FROM users WHERE email = 'user@user.com'
+) LIMIT 1;
