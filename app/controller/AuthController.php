f7fda98754046a1b3ce7746345f1dee3d4dda0bf
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-11-21 14:04:12 -0300

    agrego documentacion de metodos y corrijo manejo de formularios vacios en authenticate y register

--- app/controller/AuthController.php
+++ app/controller/AuthController.php
@@ -11,14 +11,20 @@ class AuthController{
         $this->user = new UserModel();
     }
 
+    /* Redirecciona a AuthView llamando vista de formulario de logueo */
     function login(){
         $this->view->showLoginForm();
     }
 
+    /* Redirecciona a AuthView llamando vista de formulario de registro de usuario */
     function registerForm(){
         $this->view->showRegisterForm();
     }
 
+    /*
+    Metodo de autenticacion de usuario a traves de email y password,
+    si el usuario existe carga la sesion con los datos del user y redirige a home.
+    En caso de error muestra pantalla de error con mensaje y posible accion. */
     function authenticate(){
         if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['email']) && !empty($_POST['password'])){
             $email = $_POST['email'];
@@ -32,15 +38,20 @@ class AuthController{
                 $this->authError("Verifique sus datos e intentelo nuevamente. De persistir el error, pruebe registrandose.");
             }
         }else{
-            $this->view->showLoginForm();
+            $this->authError("No es posible loguearse con datos vacíos, asegurese de completar todos los campos.");
         }
     }
 
+    /*
+    Metodo de registro de nuevo usuario a traves de email y password hasheada,
+    se crea el nuevo user, se carga la sesion con los datos del user y redirige a home.
+    En caso de error muestra pantalla de error con mensaje y posible accion. */
     function register(){
         if($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['email']) && !empty($_POST['password'])){
             $email = $_POST['email'];
             $pass = password_hash($_POST['password'], PASSWORD_BCRYPT);
             $userExist = $this->user->getUserByEmail($email);
+
             if(!isset($userExist)){
                 $this->user->addUser($email, $pass);
                 $newUser = $this->user->getUserByEmail($email);
@@ -51,16 +62,22 @@ class AuthController{
                 $this->authError("El email utilizado ya posee una cuenta, intente con un email diferente.");
             }
         }else{
-            $this->view->showRegisterForm('Data empty');
+            $this->authError("No es posible registrar un usuario con datos vacíos, asegurese de completar todos los campos.");
         }
     }
 
+    /*
+    Metodo de restriccion de acceso a contenido
+    de usuario en sesion que no sea de tipo ADMIN */
     function checkAdmin(){
         if(!isset($_SESSION['admin']) || $_SESSION['admin'] == false){
             header('Location: ' . BASE_URL . '/');
         }
     }
 
+    /*
+    Metodo de cierre y eliminacion de
+    datos de usuario en sesion y redireccion a home */
     function logout(){
         session_start();
         session_destroy();
@@ -68,16 +85,26 @@ class AuthController{
 
     }
 
+    /*
+    Metodo de carga de datos de email, ID y tipo
+    de usuario en sesion */
     function chargeSession(Object $user){
         $_SESSION['user_id'] = $user->id;
         $_SESSION['email'] = $user->email;
         $_SESSION['admin'] = $user->admin;
     }
 
+    /*
+    Metodo que chequea si el usuario
+    existente en sesion es de tipo ADMIN */
     function isAdmin(){
         return isset($_SESSION['admin']) && $_SESSION['admin'] == true;
     }
 
+    /*
+    Metodo que redirige a vista
+    de pantalla de error y muestra mensaje
+    enviado por parametro */
     function authError(String $error){
         $this->view->showError($error);
     }
