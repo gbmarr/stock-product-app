27d86bf5045663ad40bda36569d12608c13e1ef5
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-12-06 13:31:20 -0300

    implemento nuevamente metodos comentados en Model para verificar tablas vacias y realizar INSERTS en tablas de DB

--- app/model/Model.php
+++ app/model/Model.php
@@ -18,6 +18,9 @@ class Model{
         }
     }
 
+    /*
+    Metodo de despliegue de la DB utilizando ejecucion de archivo
+    SQL almacenado en la carpeta DATA.*/
     private function deploy(){
         try {
             $pdo = new PDO("mysql:host=".SQL_HOST."", SQL_USER, SQL_PASS);
@@ -26,47 +29,64 @@ class Model{
 
             $pdo->exec($sql);
 
-            // $this->database = new PDO("mysql:host=".SQL_HOST.";dbname=".SQL_DBNAME."", SQL_USER, SQL_PASS);
-            // if($this->isTableEmpty('categories') && $this->isTableEmpty('products') && $this->isTableEmpty('users')){
-            //     $this->insertData();
-            // }
+            $this->database = new PDO("mysql:host=".SQL_HOST.";dbname=".SQL_DBNAME."", SQL_USER, SQL_PASS);
+            if($this->isTableEmpty('categories') && $this->isTableEmpty('products') && $this->isTableEmpty('users')){
+                $this->insertData();
+            }
         } catch (PDOException $e) {
             echo $e->getMessage();
         }
     }
 
-    // private function isTableEmpty($table){
-    //     $sql = "SELECT COUNT(*) FROM $table";
-    //     $isCharge = $this->database->prepare($sql);
-    //     $isCharge->execute();
-    //     return $isCharge->fetchColumn() == 0;
-    // }
+    /*
+    Metodo que verifica si la cantidad de registros en una tabla determinada
+    pasada como parámetro es igual a cero, es decir, si se encuentra vacía.*/
+    private function isTableEmpty($table){
+        $sql = "SELECT COUNT(*) FROM $table";
+        $isCharge = $this->database->prepare($sql);
+        $isCharge->execute();
+        return $isCharge->fetchColumn() == 0;
+    }
 
-    // private function insertData(){
-    //     try {
-    //         $this->database->exec(
-    //             "INSERT INTO `categories` (`name`) VALUES
-    //             ('Sin categoría asignada'),
-    //             ('Electrónica'),
-    //             ('Ropa'),
-    //             ('Hogar');");
-    //         $this->database->exec(
-    //             "INSERT INTO `products` (`name`, `description`, `idcategory`, `stock`, `price`, `imgproduct`) VALUES
-    //             ('Televisor', 'Televisor LED de 42 pulgadas', 2, TRUE, 200, 'images/img-tv.jpeg'),
-    //             ('Camiseta', 'Camiseta de algodón 100%', 3, TRUE, 30.00, 'images/camiseta.jpeg'),
-    //             ('Sofá', 'Sofá de 3 plazas color gris', 4, TRUE, 500.10, 'images/sofa.jpeg'),
-    //             ('Auriculares', 'Auriculares con cancelación de ruido', 2, TRUE, 1500, 'images/auriculares.jpeg');
-    //         ");
-    //         $this->database->exec(
-    //             "INSERT INTO `users` (`name`, `surname`, `email`, `pass`, `admin`) VALUES
-    //             ('Admin', 'User', 'webadmin@admin.com', 'admin', TRUE),
-    //             ('UserComun', 'Apellido', 'user@comun.com', 'user', FALSE);"
-    //         );
-    //     } catch (Exception $e) {
-    //         echo $e->getMessage();
-    //     }
-    // }
+    /*
+    Método de inserción de registros en las diferentes tablas de la base de datos.*/
+    private function insertData(){
+        try {
+            $this->database->exec(
+                "INSERT INTO `categories` (`catname`, `catimage`) VALUES
+                ('Sin categoría asignada', 'images/imagenPorDefault.jpg'),
+                ('Electrónica', 'images/imagenPorDefault.jpg'),
+                ('Ropa', 'images/imagenPorDefault.jpg'),
+                ('Hogar', 'images/imagenPorDefault.jpg');");
 
+            $this->database->exec(
+                "INSERT INTO `products` (`prodname`, `description`, `idcategory`, `stock`, `price`, `imgproduct`) VALUES
+                ('Televisor', 'Televisor LED de 42 pulgadas', 2, TRUE, 200, 'images/img-tv.jpeg'),
+                ('Camiseta', 'Camiseta de algodón 100%', 3, TRUE, 30.00, 'images/camiseta.jpeg'),
+                ('Sofá', 'Sofá de 3 plazas color gris', 4, TRUE, 500.10, 'images/sofa.jpeg'),
+                ('Auriculares', 'Auriculares con cancelación de ruido', 2, TRUE, 1500, 'images/auriculares.jpeg');
+            ");
+
+            $stmt = $this->database->prepare(
+                "INSERT INTO `users` (`name`, `surname`, `email`, `pass`, `token`, `admin`) VALUES
+                ('Admin', 'User', 'admin@admin.com', :adminpass, :admintoken, TRUE),
+                ('UserComun', 'Apellido', 'user@comun.com', :userpass, :usertoken, FALSE);"
+            );
+            $stmt->execute([
+                ':adminpass' => '$2y$10$xHn/k/qTE8tGr1iSSe3X0OOUbkSnYGTLp7BAuIKFUAb9teWs/Ybxi',
+                ':userpass' => '$2y$10$hP1MFvzm7SpV6CbZX7GQDekSTWD0GyVtR0eN2icxbLHNM.QfQ0U4a',
+                ':admintoken' => '646fc0d751c58050fb1d81ee8f455420',
+                ':usertoken' => '80da7e7ce496db405ee6b67d87bf648d'
+            ]);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    
+    /*
+    Metodo de ejecucion de consultas SQL que recibe dos parametros:
+    consulta y parametros de reemplazo en la consulta que aparecen (?). */
     function executeQuery($query, $params = []){
         try {
             $action = $this->database->prepare($query);
