66a29f3820f4c46a80c93e6cd2bacabe2db0e094
Gabriel Marrero <gab.bmar.05@gmail.com>
2024-12-09 11:37:02 -0300

    agrego comentarios en view de categoria, modelo de user

--- app/model/UserModel.php
+++ app/model/UserModel.php
@@ -3,17 +3,29 @@ require_once 'Model.php';
 
 class UserModel extends Model{
 
+    /*
+    Metodo que obtiene registro de la tabla users mediante comparacion de
+    dato email recibido por parametro con campo de email y en caso de coincidencia lo retorna. */
     function getUserByEmail($email){
         $query = "SELECT `email`, `pass`, `admin` FROM `users` WHERE `email` = ?";
         $user = $this->executeQuery($query, [$email]);
         return $user->fetch(PDO::FETCH_OBJ);
     }
 
+    /*
+    Metodo que realiza un INSERT en tabla users. El mismo recibe email y pass
+    del nuevo usuario y lo almacena en la DB como nuevo registro de la tabla. */
     function addUser($email, $pass){
         $query = "INSERT INTO `users` (`email`, `pass`) VALUES (?, ?)";
         $this->executeQuery($query, [$email, $pass]);
     }
 
+    /*
+    Metodo de validacion de usuario, recibiendo email y pass del usuario a validar.
+    Se consulta la existencia y obtiene el usuario a partir del email y en caso de ser
+    positivo se verifica la coincidencia de passwords entre la del user almacenado en DB
+    y la recibida por parametro. De ser positiva la respuesta, se retorna el user, en caso
+    contrario se retorna NULL. */
     public function validateUser($email, $pass){
         $user = $this->getUserByEmail($email);
         if($user && password_verify($pass, $user->pass)){
--- app/view/CategoryView.php
+++ app/view/CategoryView.php
@@ -3,17 +3,30 @@ require_once 'View.php';
 
 class CategoryView extends View{
 
-    function showAllCategories($categories){
+    /*
+    Metodo que asigna tipo de usuario comun o admin segun corresponda y despliegua vista
+    de todas las categorias recibidos por parametro en formato lista. La vista para usuarios
+    ADMIN agrega columna con botones de edicion y eliminacion, y boton de registro de nueva categoria. */
+    function showAllCategories($categories, $admin){
         $this->smarty->assign('categories', $categories);
+        $this->smarty->assign('admin', $admin);
         $this->smarty->display('categories/categoryList.tpl');
     }
 
+    /*
+    Metodo muestra formulario de creacion/edicion de categorias. El metodo recibe categoria
+    por parametro. En caso de recibir una categoria cargada con datos, el formulario se
+    carga con los datos de la misma, en cualquier otro caso el formulario se muestra vacio. */
     function showCategoryForm($category = null){
         $this->smarty->assign('category', $category);
         $this->smarty->display('categories/categoryForm.tpl');
     }
 
-    function showErrorCategory(){
+    /*
+    Metodo que recibe mensaje de error en formato string y despliega
+    pantalla de error con mensaje de error de categoria. */
+    function showErrorCategory(String $error){
+        $this->smarty->assign('error', $error);
         $this->smarty->display('error/error.tpl');
     }
 }
